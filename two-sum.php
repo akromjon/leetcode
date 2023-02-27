06700644d5a2e4ff7d7ab04dec68a6565e2d3080
Akromjon <akromjon@ip-192-168-142-139.us-west-1.compute.internal>
2023-02-27 18:32:04 +0500

    first commit

---
+++ two-sum.php
@@ -0,0 +1,24 @@
+<?php
+// https://leetcode.com/problems/two-sum/
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $result=0;
+
+        echo $result;
+    }
+}
+
+$test1=new Solution();
+$test1->twoSum([2,7,11,15],9);
+
+$test2=new Solution();
+$test2->twoSum([3,2,4],6);
+
+$test3=new Solution();
+$test3->twoSum([3,3],6);
