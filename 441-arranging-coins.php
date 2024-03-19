9e20683bb2fe78c6c405af4172e75612f1cac81d
akromjon <numonov.akromjon1998@gmail.com>
2024-03-19 20:47:31 +0500

    441

---
+++ 441-arranging-coins.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * problem: https://leetcode.com/problems/arranging-coins
+ * date: 19.03.2024
+ * @param int $n
+ * @return int
+ */
+function arrangeCoins($n)
+{
+    
+    $counter=1;
+
+    while($counter<$n){
+        
+        $n=$n-$counter;
+
+        if($n>$counter){
+            $counter++;
+        }
+
+    }
+
+    return $counter;
+}
+echo arrangeCoins(100);
+// Input: n = 5
+// Output: 2
+// Explanation: Because the 3rd row is incomplete, we return 2.
+
+// Input: n = 8
+// Output: 3
+// Explanation: Because the 4th row is incomplete, we return 3.
