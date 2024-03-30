8f278c6d0a822fe965904266e8dc354644d5d80a
akromjon <numonov.akromjon1998@gmail.com>
2024-03-30 16:34:09 +0500

    36 - unsolved

---
+++ medium/16-3sum-closest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * problem: https://leetcode.com/problems/3sum-closest
+ * date: 29.03.2024
+ * @param int[] $nums
+ * @param int $target
+ * @return int
+ */
+function threeSumClosest($nums, $target)
+{
+}
+
+// Example 1:
+// Input: nums = [-1,2,1,-4], target = 1
+// Output: 2
+// Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+// Example 2:
+// Input: nums = [0,0,0], target = 1
+// Output: 0
+// Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+
+// Example 3:
+// nums =
+// [1,1,8,9,10]
+// target= 9
+// output: 10
