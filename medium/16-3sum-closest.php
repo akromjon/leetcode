b5ea27b98ea13d65a8b9025098609d9f84961948
akromjon <numonov.akromjon1998@gmail.com>
2024-05-14 21:27:29 +0500

    medium = 16

--- medium/16-3sum-closest.php
+++ medium/16-3sum-closest.php
@@ -7,9 +7,40 @@
  * @param int $target
  * @return int
  */
+
+
 function threeSumClosest($nums, $target)
 {
+    sort($nums);
+
+    $res=$nums[0]+$nums[1]+$nums[2];
+
+    for($i=0; $i<count($nums)-2; $i++){
+        $s=$i+1;
+        $e=count($nums)-1;
+
+        while($s<$e){
+            $sum=$nums[$s]+$nums[$e]+$nums[$i];
+
+            if(abs($sum-$target)<abs($res-$target)){
+                $res=$sum;
+            }
+
+            if ($sum<$target){
+                $s++;
+            }else{
+                $e--;
+            }
+
+
+        }
+    }
+
+    return $res;
+   
 }
+
+echo threeSumClosest([-1, 2, 1, -4], 1);
 
 // Example 1:
 // Input: nums = [-1,2,1,-4], target = 1
